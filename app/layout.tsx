36cf2965015eff8d473146f0e638af4d27df6af9
Tung Le <tung.le@advocado.me>
2025-05-10 13:49:26 +0700

    fix metadata

--- app/layout.tsx
+++ app/layout.tsx
@@ -10,8 +10,25 @@ const montserrat = Montserrat({
 });
 
 export const metadata: Metadata = {
-  title: "NGUYEN DUY THANH",
+  title: "NGUYỄN DUY THÀNH",
   description: "Nghệ sĩ trình diễn và biên đạo",
+  openGraph: {
+    title: "NGUYỄN DUY THÀNH",
+    description: "Nghệ sĩ trình diễn và biên đạo",
+    url: "https://www.nguyenduythanh.vn/",
+    images: [
+      {
+        url: "https://nguyenduythanh.s3.ap-southeast-1.amazonaws.com/hero.jpg",
+        secureUrl:
+          "https://nguyenduythanh.s3.ap-southeast-1.amazonaws.com/hero.jpg",
+        width: 1200,
+        height: 630,
+        alt: "Preview image for NGUYEN DUY THANH",
+      },
+    ],
+    type: "website",
+    siteName: "nguyenduythanh.vn",
+  },
 };
 
 export default function RootLayout({
